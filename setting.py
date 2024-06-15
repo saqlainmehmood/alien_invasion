b80d72ff0af5b95147123b60e1254dffbba4ccdf
Saqlain Mehmood <saqlainmehmood9@gmail.com>
2024-06-16 00:59:38 +0500

    Started project , frame rate, setting module

---
+++ setting.py
@@ -0,0 +1,10 @@
+class Setting:
+    """A class to store all settings for alien invasion"""
+
+    def __init__(self):
+        """initialize the game's setting"""
+        # screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
