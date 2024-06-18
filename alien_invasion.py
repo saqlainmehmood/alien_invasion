88347bdde6ab1bcbac9db4c3bb9aa5b07340bc5b
Saqlain Mehmood <saqlainmehmood9@gmail.com>
2024-06-18 13:46:28 +0500

    add ship.py and adds the _check_events() and _update_screen() Methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from setting import Setting
 
+from ship import Ship
+
 
 class AlienInvasion:
      """Overall class to manage game assets and behavior."""
@@ -16,24 +18,32 @@ class AlienInvasion:
           self.screen = pygame.display.set_mode(
               (self.setting.screen_width, self.setting.screen_height))
           pygame.display.set_caption("Alien Invasion")
+
+          self.ship = Ship(self)
      
      def run_game(self):
           """Start the main loop for the game"""
           while True:
-            #    watch for keyboard and mouse
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
             
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.setting.bg_color)
-
-            #  Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
 
             # set the background color
-            self.bg_color = (230, 230, 230)
+            # self.bg_color = (230, 230, 230)
+
+     def _check_events(self):
+         """respond to keypresses and mouse events."""
+         for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+     def _update_screen(self):
+            """update images on the screen and flip to new screen"""
+            self.screen.fill(self.setting.bg_color)
+            self.ship.blitme()
+
+            pygame.display.flip()
 
 if __name__ == '__main__':
     # Make game instance, and rub the game
