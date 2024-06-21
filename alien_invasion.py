b4692e0084d9c3e4f412266767ae2a9dac3e118f
Saqlain Mehmood <saqlainmehmood9@gmail.com>
2024-06-21 11:30:43 +0500

    Repopulating fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -103,6 +103,10 @@ class AlienInvasion:
              collisions = pygame.sprite.groupcollide(
                  self.bullets, self.aliens, True, True
             )
+             if not self.aliens:
+                #   Destroy existing bullets and create new fleet
+                self.bullets.empty()
+                self._create_fleet()
      
 
      def _update_aliens(self):
