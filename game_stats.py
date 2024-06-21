5d7bedecb3a2947168b7eef2a9b21c68bb3a29ae
Saqlain Mehmood <saqlainmehmood9@gmail.com>
2024-06-21 15:53:52 +0500

    High scores

--- game_stats.py
+++ game_stats.py
@@ -6,9 +6,13 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # high score should never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         """intializw statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+
         
         
--- scoreboard.py
+++ scoreboard.py
@@ -13,8 +13,9 @@ class ScoreBoard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # preparing intial score
+        # preparing intial score images
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """turn the score into a render image"""
@@ -30,6 +31,27 @@ class ScoreBoard:
         self.score_rect.right = self.score_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # Center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rec()
+        self.high_score_rect.centerx = self.screen_rect.centrex
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """Draw score to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """Check to see if theres new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+            
