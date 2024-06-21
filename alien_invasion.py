ccfdb43150eefd5d11064f17677eaa354bd6b37e
Saqlain Mehmood <saqlainmehmood9@gmail.com>
2024-06-21 12:19:25 +0500

    identifying when parts of games should run.

--- alien_invasion.py
+++ alien_invasion.py
@@ -103,9 +103,11 @@ class AlienInvasion:
           while True:
             
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+               self.ship.update()
+               self._update_bullets()
+               self._update_aliens()
+               
             self._update_screen()
             self.clock.tick(60)
 
